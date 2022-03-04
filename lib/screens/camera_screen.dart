7778d364a7c0a923bc8a356e5a2ee21290f24688
HenriNicodeme <81639151+HenriNicodeme@users.noreply.github.com>
2022-03-04 21:05:20 +0200

    Created stateful widget

---
+++ lib/screens/camera_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class camera_screen extends StatefulWidget {
+  @override
+  _camera_screenState createState() => _camera_screenState();
+}
+
+class _camera_screenState extends State<camera_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
