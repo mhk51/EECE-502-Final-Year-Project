c69d4d518191cbdfff958bc5440db01f7ed6e2ca
HenriNicodeme <81639151+HenriNicodeme@users.noreply.github.com>
2022-03-06 22:51:57 +0200

    Henri added the camera

--- lib/screens/camera_screen.dart
+++ lib/screens/camera_screen.dart
@@ -1,13 +1,68 @@
+//import 'dart:html';
 import 'package:flutter/material.dart';
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
 
-class camera_screen extends StatefulWidget {
+class CameraScreen extends StatefulWidget {
   @override
-  _camera_screenState createState() => _camera_screenState();
+  State<StatefulWidget> createState() {
+    return _CameraScreenState();
+  }
 }
 
-class _camera_screenState extends State<camera_screen> {
+class _CameraScreenState extends State<CameraScreen> {
+  var _image = File('file.txt');
+  ImagePicker picker = ImagePicker();
+  Future getImage(bool isCamera) async {
+    XFile? image;
+    if (isCamera) {
+      image = await picker.pickImage(source: ImageSource.camera);
+    } else {
+      image = await picker.pickImage(source: ImageSource.gallery);
+    }
+    //   Image.file(File(image!.path));
+    setState(() {
+//      _image = File(image!.path);
+      Image.file(File(image!.path));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Image Pick Demo'),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.insert_drive_file),
+                onPressed: () {
+                  getImage(false);
+                },
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              IconButton(
+                icon: Icon(Icons.camera_alt),
+                onPressed: () {
+                  getImage(true);
+                },
+              ),
+              _image == null
+                  ? Container()
+                  : Image.file(
+                      _image,
+                      height: 300.0,
+                      width: 300.0,
+                    ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,6 +1,7 @@
 import 'package:flutter_application_1/models/user.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/Authentication/authenticate.dart';
+import 'package:flutter_application_1/screens/camera_screen.dart';
 import 'package:flutter_application_1/screens/home_screen.dart';
 import 'package:provider/provider.dart';
 
@@ -20,7 +21,7 @@ class _WrapperState extends State<Wrapper> {
     if (user == null) {
       return const Authenticate();
     } else {
-      return const HomeScreen();
+      return CameraScreen();
     }
   }
 }
