ca5d851f0b9e2a2ab8e9ef4bfbf9a3aee20ba08b
HenriNicodeme <81639151+HenriNicodeme@users.noreply.github.com>
2022-03-21 15:27:38 +0200

    Camera Screen

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
           '/DailyLogging': (context) => const DailyLogScreen(),
           '/Settings': (context) => const SettingsForm(),
           //Henri:
-          '/CameraScreen': (context) => CameraScreen(),
+          // '/CameraScreen': (context) => CameraScreen(),
         },
       ),
     );
