257bb53410fe0f50d2a1b38e26811a0c3b62d644
Hadi Kabrit <hmk50@mail.aub.edu>
2022-03-22 12:02:45 +0200

    visual recipe ingredients +dialog for name

--- lib/screens/input_new_recipe.dart
+++ lib/screens/input_new_recipe.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_application_1/screens/logging_food/logging_food_screen.dart';
 
 class inputNewRecipe extends StatefulWidget {
@@ -9,12 +10,47 @@ class inputNewRecipe extends StatefulWidget {
 }
 
 class _inputNewRecipeState extends State<inputNewRecipe> {
+  GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Future<void> showInformationDialog(BuildContext context) async {
+    return await showDialog(
+        context: context,
+        builder: (context) {
+          final TextEditingController _textEditingController =
+              TextEditingController();
+          return AlertDialog(
+            content: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    TextFormField(
+                      validator: (value) {
+                        return value!.isNotEmpty ? null : "Invalid Field";
+                      },
+                      decoration:
+                          const InputDecoration(hintText: "Enter Recipe Name"),
+                      controller: _textEditingController,
+                    ),
+                  ],
+                )),
+            actions: <Widget>[
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text("Save"))
+            ],
+          );
+        });
+  }
+
   @override
   Widget build(BuildContext context) {
     var recipe =
         ModalRoute.of(context)!.settings.arguments as recipeIngredients;
     return SafeArea(
       child: Scaffold(
+          resizeToAvoidBottomInset: false,
           appBar: AppBar(
             backgroundColor: Colors.blue[800],
             title: const Padding(
@@ -25,23 +61,59 @@ class _inputNewRecipeState extends State<inputNewRecipe> {
           ),
           body: Center(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
+              //mainAxisAlignment: MainAxisAlignment.end,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                ElevatedButton(
-                  onPressed: () async {
-                    await Navigator.pushNamed(context, '/NewRecipeSearch',
-                        arguments: recipe);
-                  },
-                  child: const Text("Add Ingredients"),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      height: 550,
+                      child: ListView.builder(
+                          padding: const EdgeInsets.all(8),
+                          itemCount: recipe.ingredients.length,
+                          itemBuilder: (BuildContext context, int index) {
+                            return Container(
+                              height: 50,
+                              margin: EdgeInsets.all(2),
+                              color: Colors.blue,
+                              child: Center(
+                                  child: Text(
+                                '${recipe.ingredients[index].foodName}',
+                                style: TextStyle(fontSize: 18),
+                              )),
+                            );
+                          }),
+                    ),
+                  ],
                 ),
-                ElevatedButton(
-                  onPressed: () {
-                    for (int i = 0; i < recipe.ingredients.length; i++) {
-                      print(recipe.ingredients[i].foodName);
-                    }
-                  },
-                  child: const Text("Save Recipe"),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        setState(() {});
+                      },
+                      child: const Text("Refresh"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        await Navigator.pushNamed(context, '/NewRecipeSearch',
+                            arguments: recipe);
+                      },
+                      child: const Text("Add Ingredients"),
+                    ),
+                    ElevatedButton(
+                      onPressed: () async {
+                        await showInformationDialog(context);
+
+                        // for (int i = 0; i < recipe.ingredients.length; i++) {
+                        //   print(recipe.ingredients[i].foodName);
+                        // }
+                      },
+                      child: const Text("Save Recipe"),
+                    ),
+                  ],
                 ),
               ],
             ),
