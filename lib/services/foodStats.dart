175b818dcb81edc961d8a77155a7fcff49249fb1
Hadi Kabrit <hmk50@mail.aub.edu>
2022-04-19 12:06:08 +0300

    deleted comments

--- lib/services/foodStats.dart
+++ lib/services/foodStats.dart
@@ -19,52 +19,6 @@ class FoodStatsService {
     return doc.docs;
   }
 
-  // Stream<List<DocumentSnapshot>> get dailyLogsFoodClass {
-  //   var now = DateTime.now();
-  //   var lastMidnight = DateTime(now.year, now.month, now.day);
-  //   Stream<List<DocumentSnapshot>> result = userFoodStatsCollection
-  //       .where("userID", isEqualTo: uid)
-  //       .where('timeAdded', isGreaterThan: Timestamp.fromDate(lastMidnight))
-  //       .snapshots()
-  //       .map(listfromQuery);
-  //   return result;
-  // }
-
-  // FoodClass foodClassfromDoc(DocumentSnapshot doc) {
-  //   Map<String, dynamic> food = doc.get('food');
-  //   return FoodClass(
-  //       foodName: food['foodName'],
-  //       sugar: food['sugar'],
-  //       carbs: food['carbs'],
-  //       protein: food['protein'],
-  //       fat: food['fat'],
-  //       bloodSugarInc: 0);
-  // }
-
-  // Future<List<FoodClass>> getFoodLogs(String mealType) async {
-  //   var now = DateTime.now();
-  //   var lastMidnight = DateTime(now.year, now.month, now.day);
-  //   return (await userFoodStatsCollection
-  //           .where("userID", isEqualTo: uid)
-  //           .where('mealType', isEqualTo: mealType)
-  //           .where('timeAdded', isGreaterThan: Timestamp.fromDate(lastMidnight))
-  //           .get())
-  //       .docs
-  //       .map(foodClassfromDoc)
-  //       .toList();
-  // }
-
-  // Map<String, dynamic> _foodnamefromFoodClass(
-  //     FoodClass food, double multiplier) {
-  //   return {
-  //     'foodName': food.foodName,
-  //     'carbs': (food.carbs * multiplier * 100).round() / 100,
-  //     'fat': (food.fat * multiplier * 100).round() / 100,
-  //     'protein': (food.protein * multiplier * 100).round() / 100,
-  //     'sugar': (food.sugar * multiplier * 100).round() / 100,
-  //   };
-  // }
-
   Stream<List<DocumentSnapshot>> get recommendedFoodClass {
     Stream<List<DocumentSnapshot>> result = userFoodStatsCollection
         .where("count", isGreaterThan: 1)
