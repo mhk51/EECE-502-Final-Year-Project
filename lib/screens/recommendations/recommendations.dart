004694817609cb2e3b19d1a474026ad46e159e39
Mohammad Kachmar <mhk51@mail.aub.edu>
2022-05-01 16:41:27 +0300

    correction factor logic

--- lib/screens/recommendations/recommendations.dart
+++ lib/screens/recommendations/recommendations.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screens/recommendations/recommendationTile.dart';
-import 'package:flutter_application_1/services/foodStats.dart';
+import 'package:flutter_application_1/services/food_stats_service.dart';
 
 import '../../custom/loading.dart';
 import '../../services/auth.dart';
