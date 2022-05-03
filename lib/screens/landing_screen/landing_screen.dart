50c4795c5861d0e1ab1295d96a7554cfb5edef93
Hadi Kabrit <hmk50@mail.aub.edu>
2022-05-03 20:12:10 +0300

    landing screen in progress

--- lib/screens/landing_screen/landing_screen.dart
+++ lib/screens/landing_screen/landing_screen.dart
@@ -9,127 +9,112 @@ class LandingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
+    final size = MediaQuery.of(context).size;
     final scale = mockupWidth / width;
     final textScaleFactor = width / mockupWidth.toDouble();
+    print(scale);
     return SafeArea(
       child: Scaffold(
         backgroundColor: const Color.fromARGB(255, 255, 75, 58),
-        body: Stack(children: [
-          Positioned(
-            left: 51,
-            right: 95,
-            top: 82,
-            bottom: 641,
-            child: Text(
-              '''What Should I Eat?''',
-              overflow: TextOverflow.visible,
-              textAlign: TextAlign.left,
-              textScaleFactor: textScaleFactor,
-              style: const TextStyle(
-                height: 0.8683594336876502,
-                fontSize: 65.0,
-                fontFamily: 'Inria Serif',
-                fontWeight: FontWeight.w400,
-                color: Color.fromARGB(255, 255, 255, 255),
+        body: SingleChildScrollView(
+          child: Column(
+              //
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 0.05 * size.height,
+                ),
+                Container(
+                  padding: EdgeInsets.only(left: 0.04 * size.width),
+                  child: Text(
+                    '''What\nShould I\nEat?''',
+                    overflow: TextOverflow.visible,
+                    textAlign: TextAlign.left,
+                    textScaleFactor: textScaleFactor,
+                    style: const TextStyle(
+                      height: 0.8683594336876502,
+                      fontSize: 65.0,
+                      fontFamily: 'Inria Serif',
+                      fontWeight: FontWeight.w400,
+                      color: Color.fromARGB(255, 255, 255, 255),
 
-                /* letterSpacing: -1.95, */
-              ),
-            ),
-          ),
-          Positioned(
-              left: 185,
-              right: 24,
-              top: 231,
-              bottom: 471,
-              child: Image.asset(
-                "assets/images/buritto.png",
-                height: 194,
-                width: 205,
-                scale: scale,
-              )),
-          Positioned(
-              left: 12,
-              right: 172.68,
-              top: 328,
-              bottom: 307.75,
-              child: Image.asset(
-                "assets/images/burger.png",
-                height: 194,
-                width: 205,
-                scale: scale,
-              )),
-          Positioned(
-              left: 87,
-              right: 12.46,
-              top: 394,
-              bottom: 113.84,
-              child: Image.asset(
-                "assets/images/pizza.png",
-                height: 194,
-                width: 205,
-                scale: scale,
-              )),
-          Positioned(
-              left: 50,
-              right: 50,
-              // top: 747,
-              bottom: 79,
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/Signup1');
-                },
-                style: ButtonStyle(
-                  minimumSize: MaterialStateProperty.all<Size>(Size(314, 70)),
-                  backgroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
+                      /* letterSpacing: -1.95, */
                     ),
                   ),
                 ),
-                child: Text(
-                  "Get Started",
-                  textScaleFactor: textScaleFactor,
-                  overflow: TextOverflow.visible,
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(
-                    height: 0.8683594336876502,
-                    fontSize: 24.0,
-                    fontFamily: 'Inria Serif',
-                    fontWeight: FontWeight.w400,
-                    color: Color.fromARGB(255, 255, 75, 58),
-
-                    /* letterSpacing: -1.95, */
+                Container(
+                  padding: EdgeInsets.only(left: 0.04 * size.width),
+                  child: Image.asset(
+                    "assets/images/buritto.png",
+                    height: 194,
+                    width: 205,
+                    // scale: scale,
                   ),
                 ),
-              )),
-          Positioned(
-              left: 30,
-              right: 30,
-              // top: 747,
-              bottom: 30,
-              child: TextButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, "/SignIn");
-                },
-                child: Text(
-                  "Already have an Account? Log In Here",
-                  textScaleFactor: textScaleFactor,
-                  overflow: TextOverflow.visible,
-                  textAlign: TextAlign.left,
-                  style: const TextStyle(
-                    height: 0.8683594336876502,
-                    fontSize: 18.0,
-                    fontFamily: 'Inria Serif',
-                    fontWeight: FontWeight.w400,
-                    color: Color.fromARGB(255, 255, 255, 255),
+                Image.asset(
+                  "assets/images/burger.png",
+                  height: 194,
+                  width: 205,
+                  // scale: scale,
+                ),
+                Image.asset(
+                  "assets/images/pizza.png",
+                  height: 194,
+                  width: 205,
+                  // scale: scale,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/Signup1');
+                  },
+                  style: ButtonStyle(
+                    minimumSize: MaterialStateProperty.all<Size>(Size(314, 70)),
+                    backgroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                    ),
+                  ),
+                  child: Text(
+                    "Get Started",
+                    textScaleFactor: textScaleFactor,
+                    overflow: TextOverflow.visible,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      height: 0.8683594336876502,
+                      fontSize: 24.0,
+                      fontFamily: 'Inria Serif',
+                      fontWeight: FontWeight.w400,
+                      color: Color.fromARGB(255, 255, 75, 58),
 
-                    /* letterSpacing: -1.95, */
+                      /* letterSpacing: -1.95, */
+                    ),
                   ),
                 ),
-              )),
-        ]),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "/SignIn");
+                  },
+                  child: Text(
+                    "Already have an Account? Log In Here",
+                    textScaleFactor: textScaleFactor,
+                    overflow: TextOverflow.visible,
+                    textAlign: TextAlign.left,
+                    style: const TextStyle(
+                      height: 0.8683594336876502,
+                      fontSize: 18.0,
+                      fontFamily: 'Inria Serif',
+                      fontWeight: FontWeight.w400,
+                      color: Color.fromARGB(255, 255, 255, 255),
+
+                      /* letterSpacing: -1.95, */
+                    ),
+                  ),
+                ),
+              ]),
+        ),
       ),
     );
   }
