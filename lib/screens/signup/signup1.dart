f35800d14d03f626bf05980e511f1b219dd6cf0d
Hadi Kabrit <hmk50@mail.aub.edu>
2022-05-03 15:40:00 +0300

    labels fixed

--- lib/screens/signup/signup1.dart
+++ lib/screens/signup/signup1.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../Authentication/registration_class.dart';
 
 class SignUp1 extends StatefulWidget {
   const SignUp1({Key? key}) : super(key: key);
@@ -8,8 +11,11 @@ class SignUp1 extends StatefulWidget {
 }
 
 class _SignUp1State extends State<SignUp1> {
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
+    RegistrationClass registrationClass =
+        Provider.of<RegistrationClass>(context);
     return SafeArea(
       child: Scaffold(
           backgroundColor: const Color.fromARGB(242, 242, 242, 242),
@@ -81,60 +87,63 @@ class _SignUp1State extends State<SignUp1> {
                     ),
                   ]),
                 ),
-                Column(
-                  children: [
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 50, right: 50, top: 60),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'First Name',
+                Form(
+                  key: _formKey,
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 50, right: 50, top: 60),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'First Name',
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsets.only(left: 50, right: 50, top: 50),
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          labelText: 'Family Name',
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 50, right: 50, top: 50),
+                        child: TextFormField(
+                          decoration: const InputDecoration(
+                            labelText: 'Family Name',
+                          ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(bottom: 40, top: 60),
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ButtonStyle(
-                          minimumSize: MaterialStateProperty.all<Size>(
-                              const Size(314, 70)),
-                          backgroundColor: MaterialStateProperty.all<Color>(
-                              const Color.fromARGB(255, 255, 75, 58)),
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 40, top: 60),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          style: ButtonStyle(
+                            minimumSize: MaterialStateProperty.all<Size>(
+                                const Size(314, 70)),
+                            backgroundColor: MaterialStateProperty.all<Color>(
+                                const Color.fromARGB(255, 255, 75, 58)),
+                            shape: MaterialStateProperty.all<
+                                RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(30.0),
+                              ),
+                            ),
+                          ),
+                          child: const Text(
+                            "Next",
+                            // textScaleFactor: textScaleFactor,
+                            overflow: TextOverflow.visible,
+                            textAlign: TextAlign.left,
+                            style: TextStyle(
+                              height: 0.8683594336876502,
+                              fontSize: 24.0,
+                              fontFamily: 'Inria Serif',
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+
+                              /* letterSpacing: -1.95, */
                             ),
                           ),
                         ),
-                        child: const Text(
-                          "Next",
-                          // textScaleFactor: textScaleFactor,
-                          overflow: TextOverflow.visible,
-                          textAlign: TextAlign.left,
-                          style: TextStyle(
-                            height: 0.8683594336876502,
-                            fontSize: 24.0,
-                            fontFamily: 'Inria Serif',
-                            fontWeight: FontWeight.w400,
-                            color: Colors.white,
-
-                            /* letterSpacing: -1.95, */
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
+                      )
+                    ],
+                  ),
                 )
               ],
             ),
